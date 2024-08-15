bd0c400dcf45037ff70cbd587df6b64208abc415
TI-RP3 <ti@rp3combustiveis.com>
2024-08-15 14:46:47 -0300

    Segunda versão
    Melhorias aplicadas na identificação e aplicação do DDI

--- ProjectOneVersion.py
+++ ProjectOneVersion.py
@@ -9,9 +9,12 @@ def formatacaoNumero(numero): #função que vai fazer as validações e a format
            {"RJ": ["21", "22", "24"]}
            ]
 
-    for i in numero: #laço que passa por toda a lista de numeros
-        if not i.startswith(DDI): #verifica se a inicial dos números iniciam ou não com +55
-            num = DDI + numero #caso não inicie, inlcua
+    if numero.startswith(DDI): #verifica se a inicial dos números iniciam ou não com +55
+        num = numero
+        print(f"DDI {DDI} encontrado!")
+    else:
+        num = DDI + numero
+        print(f"DDI {DDI} adicionado!")
 
     prefixo = num[3:5] #pego apenas os dois números que indicam o DDD
 
@@ -19,6 +22,7 @@ def formatacaoNumero(numero): #função que vai fazer as validações e a format
         for estado, codigo in item.items(): #laço que captura a chave e o valor do dicionario
             if prefixo in codigo: # verifica se o prefixo está incluído na lista de ddd
                 print(f"Seu número é: {num}  Estado: {estado}") #printa o resultado
+                return True
 
 
     return False
@@ -29,6 +33,6 @@ def formatacaoNumero(numero): #função que vai fazer as validações e a format
 
 #ESCOPO PRINCIPAL
 
-numero = ["11923456789", "71923456789"]
+numero = ["11923456789", "71923456789", "+5571923456788" ]
 for num in numero:
     funcao = formatacaoNumero(num)
